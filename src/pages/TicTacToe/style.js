3a5d04a441500bbdf44d3989288326787bf1e531
Vito Breda <vito_odoni@hotmail.com>
2020-10-03 14:27:41 -0300

    add styled components to TicTacToe

--- src/pages/TicTacToe/style.js
+++ src/pages/TicTacToe/style.js
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
 import constants from 'expo-constants';
+import styled from 'styled-components';
 
 export const color = {
   blue: '#2980b9',
@@ -8,88 +8,94 @@ export const color = {
   primaryGray: '#ced6e0',
 };
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: color.primaryGray,
-    justifyContent: 'flex-start',
-  },
-  wrapper: {
-    flex: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-  },
-  header: {
-    paddingTop: constants.statusBarHeight,
-    width: '100%',
-    backgroundColor: '#f39c12',
-    flexDirection: 'row',
-    paddingLeft: 10,
-  },
-  headerText: {
-    fontWeight: 'bold',
-    fontSize: 17,
-    color: '#2c3e50',
-    marginBottom: 10,
-    marginLeft: 10,
-  },
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${color.primaryGray};
+    justify-content: flex-start;
+`;
 
-  tictactoe: {
-    height: 243,
-    width: 243,
-  },
-  square: {
-    width: 80,
-    height: 80,
-    backgroundColor: color.primaryGray,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#576574',
-  },
-  img: {
-    height: 70,
-    width: 70,
-    resizeMode: 'contain',
-    opacity: 0.8,
-  },
-  scoreWrapper: {
-    height: 55,
-    marginBottom: 10,
-    backgroundColor: color.primaryWhite,
-    borderRadius: 5,
-    borderWidth: 2,
-    borderColor: '#dcdde1',
-    marginBottom: 30,
-  },
-  score: {
-    flexDirection: 'row',
-  },
-  scoreTitle: {
-    width: 120,
-    textAlign: 'center',
-    fontSize: 17,
-    fontWeight: 'bold',
-  },
-  scoreText: {
-    width: 120,
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  winnerWrapper: {
-    height: '20%',
-    justifyContent: 'space-around',
-    marginBottom: '10%',
-  },
-  winnerDisplay: {
-    justifyContent: 'center',
-  },
-  winnerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    textAlign: 'center',
-  },
-});
+export const Wrapper = styled.View`
+    flex: 1;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+export const Header = styled.View`
+    padding-top: ${constants.statusBarHeight}px;
+    width: 100%;
+    background-color: #f39c12;
+    flex-direction: row;
+    padding-left: 10px;
+`;
+  
+export const HeaderText = styled.Text`
+    font-weight: bold;
+    font-size: 17px;
+    color: #2c3e50;
+    margin-bottom: 10px;
+    margin-left: 10px;
+`;
 
-export default style;
+export const Tictactoe = styled.View`
+    height: 243px;
+    width: 243px;
+`;
+  
+export const Square = styled.View`
+    width: 80px;
+    height: 80px;
+    background-color: ${color.primaryGray};
+    justify-content: center;
+    align-items: center;
+    border-color: #576574;
+`;
+
+export const Img = styled.View`
+    height: 70px;
+    width: 70px;
+    opacity: 0.8px;
+`;
+
+export const ScoreWrapper = styled.View`
+    height: 55px;
+    margin-bottom: 10px;
+    background-color: ${color.primaryWhite};
+    border-radius: 5px;
+    border-width: 2px;
+    border-color: #dcdde1;
+    margin-bottom: 30px;
+`;
+
+export const Score = styled.View`
+    flex-direction: row;
+`;
+
+export const ScoreTitle = styled.Text`
+    width: 120px;
+    text-align: center;
+    font-size: 17px;
+    font-weight: bold;
+`;
+
+export const ScoreText = styled.Text`
+    width: 120px;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+export const WinnerWrapper = styled.View`
+    height: 20%;
+    justify-content: space-around;
+    margin-bottom: 10%;
+`;
+
+export const WinnerDisplay = styled.View`
+    justify-content: center;
+`;
+
+export const WinnerText = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    text-transform: uppercase;
+    text-align: center;
+`;
